9177e91ecf839d330802cec6b1357a6807e1fc31
Aamir Khan Maarofi <32407958+Aamir-Khan-Maarofi@users.noreply.github.com>
2022-07-12 20:25:31 +0500

    training completed

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from models import db, UserModel
+import pandas as pd
+import secrets
+import joblib
 
 app = Flask(__name__)
+app.secret_key = secrets.token_bytes(32)
 db.init_app(app)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hdp_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -17,21 +23,75 @@ def analysis():
 
 @app.route('/clean_data', methods=["POST"])
 def clean_data():
-    print(" <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< WHAT THE HELL IS GOING ON HERE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
     if request.method == "POST":
-        data = request.get_json(force=True)
-        csv_data = ''
-       
-        with open('./data.csv', 'w') as data_file:
-            csv_data += ",".join(data[0].keys()) + '\n' # Headers in string format e.g. A, B, C, D, E, F 
-            for row in data:
-                csv_data += ",".join(row.values()) + '\n'
+        try:
+            data = request.get_json(force=True)
+            csv_data = ''
+        
+            with open('./data.csv', 'w') as data_file:
+                for row in data:
+                    csv_data += ",".join(row.values()) + '\n'
+                
+                data_file.write(csv_data)
+
+            heart = pd.read_csv('./data.csv', names=data[0].keys())
+
+            heart["ST depression"] = heart["ST depression"].astype("int64")
             
-            data_file.write(csv_data)
-    
-        print(csv_data)
-        return csv_data
+            min_max = MinMaxScaler()
+            columns_to_scale = ['Age', 'BP', 'Cholesterol', 'Max HR', 'Thallium']
+            heart[columns_to_scale ] = min_max.fit_transform(heart[columns_to_scale ])
+
+            x = heart[['Age', 'BP', 'Cholesterol', 'Max HR', 'Thallium']]
+            y = heart['Heart Disease']
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.30)
+            
+            data = {
+                'headers' : ['Age', 'BP', 'Cholesterol', 'Max HR', 'Thallium', 'Heart Disease'],
+                'clean_data' : (x.join(y)).to_dict('records'), 
+                'train_data' : (x_train.join(y_train)).to_dict('records'),
+                'test_data' : (x_test.join(y_test)).to_dict('records')
+            }
+
+            return {'message': 'Success', 'data': data, 'status': 200}
+        except Exception as error:
+            return {'message': 'Failed - Internal Server Error - {}'.format(error), 'status': 500}
     else:
-        return "NOT SO GREAT"
+        return {'message': 'Method not allowed, only POST method is supported', 'status': 404}
+
+@app.route('/train_model', methods=["POST"])
+def train_model():
+
+    if request.method == "POST":
+        try:
+            data = request.get_json(force=True)
+            train_data = pd.DataFrame(data['train_data'])
+            test_data = pd.DataFrame(data['test_data'])
+            x_train, y_train = train_data[['Age', 'BP', 'Cholesterol', 'Max HR', 'Thallium']] , train_data['Heart Disease']
+            x_test, y_test= test_data[['Age', 'BP', 'Cholesterol', 'Max HR', 'Thallium']], test_data['Heart Disease']
+
+            model = LogisticRegression()
+            model.fit(x_train, y_train)
+            y_pred = model.predict(x_test)
+
+            print('<=====================================================================>')
+            print("Accuracy= ", model.score(x_test, y_test)*100)
+            print('<=====================================================================>')
+
+            joblib.dump(model, "model.pkl")
+
+            return {
+                'message': 'Successfully Trained the Logistic Regression model',
+                'data': 'Model Score: {}'.format(model.score(x_test, y_test)*100),
+                'status': 200
+                }
+
+        except Exception as error:
+            return {'message': 'Failed - Internal Server Error - {}'.format(error), 'status': 500}
+    else:
+        return {'message': 'Method not allowed, only POST method is supported', 'status': 404}
+
+
 if __name__ == "__main__":
     app.run('localhost', 9999, debug=True, use_reloader=True)
